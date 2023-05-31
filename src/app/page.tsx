99c220374a84ed4d3dd3ada694f72804058a7c25
Anunay Sinha <anusu90@gmail.com>
2023-05-31 15:04:36 +0530

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import FormStepper from "../components/FormStepper";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <FormStepper />
+      <div>
+        <Link href="/forms/formik">Formik</Link>
+      </div>
+      <div>
+        <Link href="/forms/react-forms">React Forms</Link>
+      </div>
     </>
   );
 }
